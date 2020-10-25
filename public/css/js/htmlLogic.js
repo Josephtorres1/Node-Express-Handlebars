cc0d7d2e2a4820072f66c9ef49292ab9effbf28e
Joseph Torres <simply.joseph00@gmail.com>
2020-10-25 14:32:39 -0400

    updated htmlLogic file

---
+++ public/css/js/htmlLogic.js
@@ -0,0 +1,34 @@
+$(function () {
+  $(".devourBtn").on("click", function (event) {
+    var id = $(this).data("id");
+    var newDevourState = {
+      devoured: true,
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState,
+    }).then(function () {
+      location.reload();
+    });
+  });
+  $(".addBurger").on("click", function (event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#burgerName").val().trim(),
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      location.reload();
+    });
+  });
+  $(".deleteBtn").on("click", function (event) {
+    event.preventDefault();
+    $.ajax("/api/burgers/all", {
+      type: "DELETE",
+    }).then(function () {
+      location.reload();
+    });
+  });
+});
