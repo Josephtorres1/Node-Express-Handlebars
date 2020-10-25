646d60df9c5cad4204bd35efae428af9c5349f52
Joseph Torres <simply.joseph00@gmail.com>
2020-10-25 15:46:40 -0400

    added png

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const pw = require("../pw");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -7,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: process.env.DB_PASSWORD,
+    password: pw,
     database: "burger_db",
   });
 }
